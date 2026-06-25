6e37f1a6d0fa0cea7f2f290de7aac6edef539302
Alex P <apputtre@gmail.com>
2026-06-25 00:36:11 -0400

    Began implementation of WasRun

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WasRun test = new WasRun("testMethod");
+        Console.WriteLine(test.wasRun);
+        test.testMethod();
+        Console.WriteLine(test.wasRun);
+    }
+}
+
+
+class WasRun
+{
+    public bool wasRun;
+
+    public WasRun(string name)
+    {
+        wasRun = false;
+    }
+
+    public void testMethod()
+    {
+        wasRun = true;
+    }
+}
